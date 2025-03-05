e98533ed2973143d28a2177d01f4d75a9f15ae36
Andrea.Parrini.Lavoro <andrea.parrini26@gmail.com>
2025-03-05 11:11:02 +0100

    import useSelector e useDispatch
    import hooks and use to manage state of counter
    add class in counter.module.css

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,30 @@
+import {useSelector, useDispatch} from 'react-redux'
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const dispatch= useDispatch();
+  const counter = useSelector(state=>state.counter); 
+  //useSelector imposta in automatico un "abbonamento" a questo componente, cosi da riaggiornarlo sempre con l'ultimo valore dello stato igni volta che cambia
+
   const toggleCounterHandler = () => {};
+
+  const handlerIncrement=() => {
+    dispatch({type: 'INCREMENT'});
+  };
+  const handlerDecrement=() => {
+    dispatch({type: 'DECREMENT'});
+  };
+
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={handlerIncrement}>Increment</button>
+        <button onClick={handlerDecrement}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
