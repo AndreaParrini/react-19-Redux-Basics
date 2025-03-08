829847e204bbbe250f6fcd271bba0f472d009f97
Andrea.Parrini.Lavoro <andrea.parrini26@gmail.com>
2025-03-08 17:51:04 +0100

    use redux with class-based components

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,10 +2,11 @@ import {useSelector, useDispatch} from 'react-redux'
 
 import classes from './Counter.module.css';
 
+
 const Counter = () => {
   const dispatch= useDispatch();
   const counter = useSelector(state=>state.counter); 
-  //useSelector imposta in automatico un "abbonamento" a questo componente, cosi da riaggiornarlo sempre con l'ultimo valore dello stato igni volta che cambia
+  //useSelector imposta in automatico un "abbonamento" a questo componente, cosi da riaggiornarlo sempre con l'ultimo valore dello stato ogni volta che cambia
 
   const toggleCounterHandler = () => {};
 
@@ -29,5 +30,52 @@ const Counter = () => {
     </main>
   );
 };
-
 export default Counter;
+
+/* 
+PER USARE I CLASS-BASED COMPONENTE SI DEVONO IMPORTARE 
+import { Component } from 'react';
+import {connect} from 'react-redux'
+
+
+class Counter extends Component{
+
+  handlerIncrement(){
+    this.props.increment();
+  }
+
+  handlerDecrement(){
+    this.props.decrement();
+  }
+
+  toggleCounterHandler(){}
+
+  render(){
+    return (
+      <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{this.props.counter}</div>
+      <div>
+        <button onClick={this.handlerIncrement.bind(this)}>Increment</button>
+        <button onClick={this.handlerDecrement.bind(this)}>Decrement</button>
+      </div>
+      <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+    </main>
+    )
+  }
+}
+
+const mapStateToProps = state=> {
+  return{
+    counter:state.counter
+  }
+}
+
+const mapDispatchToProps= dispatch=> {
+  return {
+    increment:  dispatch({type: 'INCREMENT'}),
+    decrement: dispatch({type: 'DECREMENT'})
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter); */
